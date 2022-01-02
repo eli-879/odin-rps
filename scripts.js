9211253c8d19befbe19e9a28542f4bf0f714d9e3
Eric Li <ericxli2001@gmail.com>
2022-01-02 21:31:42 +1300

    updated script to work with button

--- scripts.js
+++ scripts.js
@@ -10,10 +10,28 @@ let playRound = (playerSelection, computerSelection) => {
     let cs = computerSelection.toLowerCase();
     let ps = playerSelection.toLowerCase();
 
+    const resultText = document.querySelector(".result-text");
+    const computerScore = document.querySelector(".computer-score");
+    const humanScore = document.querySelector(".human-score");
+    
+
+    console.log(cs, ps);
+
     if (cs == "rock") {
-        if (ps == "paper") return [`You Win! ${ps} beats ${cs}`, "W"];
-        else if (ps == "rock") return ["You Tie!", "T"];
-        else if (ps == "scissors") return [`You Lose! ${cs} beats ${ps}`, "L"];
+        if (ps == "paper") {
+            resultText.textContent = `You Win! ${ps} beats ${cs}`;
+            let human_score_updated = parseInt(humanScore.textContent) + 1;
+            humanScore.textContent = human_score_updated;
+        }
+        else if (ps == "rock") {
+            resultText.textContent = `You Tie! ${ps} beats ${cs}`;
+        } 
+        else if (ps == "scissors") {
+            resultText.textContent = `You Win! ${ps} beats ${cs}`;
+            let computer_score_updated = parseInt(computerScore.textContent) + 1;
+            computerScore.textContent = computer_score_updated;
+        }
+        
     }
     else if (cs == "paper") {
         if (ps == "scissors") return [`You Win! ${ps} beats ${cs}`, "W"];
@@ -31,18 +49,24 @@ let game = () => {
     let playerScore = 0;
     let computerScore = 0;
 
-    for (let i = 0; i < 5; i++) {
+    const computerSelection = computerPlay();
+    
+    const buttons = document.querySelectorAll('.option');
+    console.log(buttons);
+    buttons.forEach((button) => {
+        button.addEventListener("click", function() {
+            playRound(button.textContent, computerSelection)
+        });
+    
+    })
 
-        const playerSelection = prompt("Enter your choice");
-        const computerSelection = computerPlay();
-
-        let result = playRound(playerSelection, computerSelection);
-        if (result[1] == "W") playerScore += 1;
-        else if (result[1] == "L") computerScore += 1;
-
-        console.log(result[0]);
-        console.log(playerScore, computerScore);
-    }
+    let result = playRound(playerSelection, computerSelection);
+    if (result[1] == "W") playerScore += 1;
+    else if (result[1] == "L") computerScore += 1;
+    console.log(result[0]);
+    console.log(playerScore, computerScore);
+    
+    
 
 }
 
